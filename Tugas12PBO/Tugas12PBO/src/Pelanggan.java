e9e776dcc2ed64f48fa4819e88c8022facd1dba5
Dzz24 <121357709+Dzz24@users.noreply.github.com>
2023-12-06 23:41:58 +0700

    Update Pelanggan.java

--- Tugas12PBO/Tugas12PBO/src/Pelanggan.java
+++ Tugas12PBO/Tugas12PBO/src/Pelanggan.java
@@ -12,12 +12,20 @@ public class Pelanggan extends InfoLengkap {
         noFaktur = dataString.next();
         System.out.println("Masukkan nama pelanggan : ");
         nama = dataString.next();
+        System.out.println("Masukkan noHp pelanggan : ");
+        noHp = dataString.next();
+        System.out.println("Masukkan alamat pelanggan : ");
+        alamat = dataString.next();
+        System.out.println("Masukkan kode barang : ");
+        kodeBarang = dataString.next();
         System.out.println("Masukkan nama barang : ");
         namaBarang = dataString.next();
         System.out.println("Masukkan harga barang : ");
         hargaBarang = dataInt.nextInt();
         System.out.println("Masukkan jumlah barang : ");
         jumlahBarang = dataInt.nextInt();
+        System.out.println("Masukkan nama Kasir : ");
+        namaKasir = dataString.next();
         dataString.close();
         dataInt.close();
         
@@ -36,14 +44,23 @@ public class Pelanggan extends InfoLengkap {
         System.out.println(day.format(date));
         System.out.println(hour.format(date));
         System.out.println("No Faktur : " + noFaktur);
+        System.out.println("--------DATA PELANGGAN-------");
         System.out.println("Nama Pelanggan : " + nama);
+        System.out.println("noHp : " + noHp);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("+++++++++++++++++++++++++++++");
+        System.out.println("----DATA PEMBELIAN BARANG----");
+        System.out.println("Kode Barang : " + kodeBarang);
         System.out.println("Nama Barang : " + namaBarang);
         System.out.println("Harga : " + hargaBarang);
         System.out.println("Jumlah : " + jumlahBarang);
         System.out.println("Total Bayar : " + this.totalBayar);
-        System.out.println("Uppercase: " + nama.toUpperCase() );
+        System.out.println("+++++++++++++++++++++++++++++");
+        System.out.println("Kasir : " + namaKasir);
+        System.out.println("Uppercase : " + nama.toUpperCase() );
         System.out.println("length\t: " + nama.length() );
         System.out.println("equals\t: " + day.equals(hour) );
+        System.out.println("-----------------------------");
     }
     
 
